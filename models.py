dbdbbff7d4fb269500c4c1bec5cc14585f17070d
julienbanze <julienbanze42@gmail.com>
2026-03-30 09:42:24 +0200

    Update models.py

--- models.py
+++ models.py
@@ -8,35 +8,14 @@ class Utilisateur(db.Model):
     nom = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(20), nullable=False) # 'Enseignant' / 'Eleve'
-    classe = db.Column(db.String(50))
-    matricule = db.Column(db.String(50))
-    points = db.Column(db.Integer, default=0) # Système de récompense
+    role = db.Column(db.String(20), nullable=False) # 'Admin', 'Enseignant', 'Eleve'
+    
+    # Champs spécifiques
+    classe = db.Column(db.String(50))      # Pour élèves et profs
+    matiere = db.Column(db.String(100))    # Pour profs
+    matricule = db.Column(db.String(50))   # Pour élèves
+    telephone = db.Column(db.String(20))
+    date_inscription = db.Column(db.DateTime, default=datetime.utcnow)
 
-class Cours(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titre = db.Column(db.String(150), nullable=False)
-    description = db.Column(db.Text)
-    fichier = db.Column(db.String(200))
-    classe_cible = db.Column(db.String(50))
-    enseignant_id = db.Column(db.Integer, db.ForeignKey('utilisateur.id'))
-
-class Devoir(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titre = db.Column(db.String(100))
-    contenu = db.Column(db.Text)
-    date_limite = db.Column(db.DateTime)
-    cours_id = db.Column(db.Integer, db.ForeignKey('cours.id'))
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    valeur = db.Column(db.Float)
-    eleve_id = db.Column(db.Integer, db.ForeignKey('utilisateur.id'))
-    devoir_id = db.Column(db.Integer, db.ForeignKey('devoir.id'))
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    expediteur_id = db.Column(db.Integer, db.ForeignKey('utilisateur.id'))
-    destinataire_id = db.Column(db.Integer, db.ForeignKey('utilisateur.id'))
-    contenu = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    def __repr__(self):
+        return f'<Utilisateur {self.nom}>'
